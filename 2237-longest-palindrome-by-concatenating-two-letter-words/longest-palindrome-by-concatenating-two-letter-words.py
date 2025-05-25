0394687a3d3cd663c1fab8347a59746dd42d5364
Sadakopa. Ramakrishnan <71075283+imstrk04@users.noreply.github.com>
2025-05-25 11:14:46 +0530

    Time: 51 ms (70.04%) | Memory: 35.7 MB (12.27%) - LeetSync

---
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestPalindrome(self, words: List[str]) -> int:
+        map = Counter(words)
+        count = 0
+        alreadyPalindrome = 0
+        for w, freq in map.items():
+            s = w[::-1]
+            if w == s:
+                count += (freq // 2) * 4
+                if freq % 2:
+                    alreadyPalindrome = 1
+            elif w < s and s in map:
+                count += min(freq, map[s]) * 4
+        
+        return count + alreadyPalindrome * 2
