bd38c298e511fa1052301178c5612f5c985071cd
Sadakopa. Ramakrishnan <71075283+imstrk04@users.noreply.github.com>
2025-10-14 20:04:29 +0530

    Time: 20 ms (36.37%) | Memory: 19.4 MB (18.70%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,20 +1,23 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height: return 0
+        if not height:
+            return 0
+        n = len(height)
+        prefixMax = [-1] * len(height)
+        suffixMax = [-1] * len(height)
 
-        l, r = 0, len(height) - 1
-        leftMax, rightMax = height[l], height[r]
+        prefixMax[0] = height[0]
+        suffixMax[n - 1] = height[-1]
 
-        res = 0
-
-        while l < r:
-            if leftMax < rightMax:
-                l += 1
-                leftMax = max(leftMax, height[l])
-                res += leftMax - height[l]
-            else:
-                r -= 1
-                rightMax = max(rightMax, height[r])
-                res += rightMax - height[r]
+        for i in range(1,len(height)):
+            prefixMax[i] = max(prefixMax[i-1], height[i])
         
-        return res
+        for i in range(len(height) - 2, -1, -1):
+            suffixMax[i] = max(suffixMax[i+1],height[i])
+        
+        total = 0
+        for i in range(n):
+            if height[i] < prefixMax[i] and height[i] < suffixMax[i]:
+                total += min(prefixMax[i], suffixMax[i]) - height[i]
+        
+        return total
