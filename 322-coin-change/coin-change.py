8e28d9b470a24f5673a2159c96a4ebe9fe5f0cfd
Sadakopa. Ramakrishnan <71075283+imstrk04@users.noreply.github.com>
2025-10-21 12:12:22 +0530

    Time: 1199 ms (13.00%) | Memory: 57.4 MB (5.01%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,13 +1,24 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        
-        dp = [float('inf')] * (amount + 1)
+        n = len(coins)
+        dp = [[-1 for _ in range(amount + 1)] for _ in range(n)]
+        def f(ind, target):
+            if ind == 0:
+                if target % coins[0] == 0:
+                    return target // coins[0]
+                else:
+                    return float('inf')
+            
+            if dp[ind][target] != -1:
+                return dp[ind][target]
 
-        dp[0] = 0 # 0 coins needed to make 0
-
-        for amt in range(1, amount + 1):
-            for coin in coins:
-                if amt - coin >= 0:
-                    dp[amt] = min(dp[amt], dp[amt - coin] + 1)
+            notTake = f(ind - 1, target)
+            take = float('inf')
+            if target >= coins[ind]:
+                take = 1+ f(ind, target - coins[ind]) # stay here till u can take the coin as many times as u can till the if condition fails
+            
+            dp[ind][target] = min(take, notTake)   
+            return dp[ind][target]
         
-        return dp[amount] if dp[amount] != float('inf') else - 1
+        ans = f(n - 1, amount)
+        return -1 if ans == float('inf') else ans
