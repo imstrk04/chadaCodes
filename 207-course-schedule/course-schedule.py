68850c3e42ad297398409c6de480626a5855ad09
Sadakopa. Ramakrishnan <71075283+imstrk04@users.noreply.github.com>
2025-10-23 09:04:18 +0530

    Time: 4 ms (67.55%) | Memory: 19.4 MB (31.20%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,38 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        g= collections.defaultdict(list)
+        courses = prerequisites
+
+        for a, b in courses:
+            g[a].append(b)
+        
+        UNVISITED = 0
+        VISITING = 1
+        VISITED = 2
+
+        states = [UNVISITED] * numCourses
+
+        def dfs(node):
+            state = states[node]
+
+            if state == VISITED:
+                return True
+            elif state == VISITING:
+                return False
+            
+            else:
+                states[node] = VISITING
+
+                for nei in g[node]:
+                    if not dfs(nei):
+                        return False
+                
+                states[node] = VISITED
+            
+            return True
+        
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        
+        return True
